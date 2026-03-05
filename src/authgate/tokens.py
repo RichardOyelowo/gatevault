aedc05462ba9fdb237e583476990e77ced389bf6
Richard <richardadebowale.oye@gmail.com>
2026-03-05 18:47:22 -0500

    full token generation and decoding configuration

--- src/authgate/tokens.py
+++ src/authgate/tokens.py
@@ -4,28 +4,47 @@ from .exceptions import TokenError, TokenExpiredError, InvalidTokenError, TokenD
 
 
 class TokenManager:
-    def __init__(self, secret_key: str, acesss_expiry_minutes: int, access_expiry_days: int) -> str:
+    def __init__(self, secret_key: str, access_expiry_minutes: int, refresh_expiry_days: int) -> None:
         self.secret_key = secret_key
-        self.access_expiry_minutes = acesss_expiry_minutes
-        self.access_expiry_days = access_expiry_days
+        self.access_expiry = access_expiry_minutes
+        self.refresh_expiry = refresh_expiry_days
 
 
     def _create_token(self, user_id: int, token_type: str, exp: timedelta, **kwargs) -> str:
         payload = {
             "user_id": user_id,
-            "exp": datetime.utcnow() + exp,
+            "exp": datetime.now(timezone.utc) + exp,
             "type": token_type,
             **kwargs
         }
-
-        return jwt.encode(payload, secret_key, "H256")
+        return jwt.encode(payload, self.secret_key, "HS256")
 
 
     def create_access_token(self, user_id: int, **kwargs) -> str:
 
-        return self._create_token(user_id, token_type="access", self.access_expiry_minutes, **kwargs)
+        return self._create_token(user_id, "access", timedelta(minutes=self.access_expiry), **kwargs)
 
 
     def create_refresh_token(self, user_id: int, **kwargs) -> str:
         
-        return self._create_token(user_id, token_type="refresh", self.access_expiry_days, **kwargs)
+        return self._create_token(user_id, "refresh", timedelta(days=self.refresh_expiry), **kwargs)
+
+
+    def decode_token(self, token: str) -> dict:
+    
+        try:
+            payload = jwt.decode(token, self.secret_key, algorithms=["HS256"])
+
+        # invalid/malformed token
+        except jwt.DecodeError:
+            raise TokenDecodeError
+        
+        # expired token
+        except jwt.ExpiredSignatureError:
+            raise TokenExpiredError
+        
+        # invalid secret_key
+        except jwt.InvalidTokenError:
+            raise InvalidTokenError
+        
+        return payload
