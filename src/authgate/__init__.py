55c08d87ce42f10c7f0828f0e6909ae7d0c7d39b
Richard <richardadebowale.oye@gmail.com>
2026-03-07 13:29:41 -0500

    added exeptions for import

--- src/authgate/__init__.py
+++ src/authgate/__init__.py
@@ -3,3 +3,8 @@ from .oauth2 import OAuthHandler
 from .tokens import TokenManager
 from .warnings import ShortKeyWarning
 from .hashing import hash_password, verify_password
+from .exceptions import (
+        TokenError, TokenExpiredError, InvalidTokenError, TokenDecodeError, 
+        GuardError, InvalidCredentialsError, UnauthorizedError, 
+        AuthgateError, HashingError
+)
